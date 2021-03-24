b105194f548c58b466445e7282f0cb6005b649f0
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-03-24 00:19:49 -0700

    Set tickrate to 128

--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -ue
 
-/opt/game/srcds_run -game csgo +map de_dust2 -strictbindport
+/opt/game/srcds_run \
+    -game csgo \
+    -tickrate 128 \
+    -strictbindport \
+    +map de_dust2
